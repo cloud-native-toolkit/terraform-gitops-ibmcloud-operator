2e9ef71b363b124a785cc61416faf38f5bca0955
balasgit <bala@us.ibm.com>
2022-09-19 14:30:01 -0500

    Fix compile errors

--- version.tf
+++ version.tf
@@ -1,10 +1,12 @@
 terraform {
   required_version = ">= 0.15.0"
-}
 
-required_providers {
-  gitops = {
-    source = "cloud-native-toolkit/gitops"
-    version = ">= 0.1.7"
+  required_providers {
+    gitops = {
+      source = "cloud-native-toolkit/gitops"
+      version = ">= 0.1.7"
+    }
   }
 }
+
+
